e25d43d88bf331f76770157fd3ae19941c1a4939
mrt <sam@gmail.com>
2026-03-27 07:31:17 -0400

    update

--- giftlink-backend/app.js
+++ giftlink-backend/app.js
@@ -22,29 +22,16 @@ connectToDatabase().then(() => {
 app.use(express.json());
 
 // Route files
-// Gift API Task 1: import the giftRoutes and store in a constant called giftroutes
 const giftRoutes = require('./routes/giftRoutes');
-
-// Search API Task 1: import the searchRoutes and store in a constant called searchRoutes
 const searchRoutes = require('./routes/searchRoutes');
-
-// auth API
-const authRoutes = require('./routes/authRoutes');
-
 const pinoHttp = require('pino-http');
 const logger = require('./logger');
 
 app.use(pinoHttp({ logger }));
 
 // Use Routes
-// Gift API Task 2: add the giftRoutes to the server by using the app.use() method.
-app.use('/api/gifts',giftRoutes);
-
-// Search API Task 2: add the searchRoutes to the server by using the app.use() method.
+app.use('/api/gifts', giftRoutes);
 app.use('/api/search', searchRoutes);
-
-// Auth API : add the authRoutes to the server
-app.use('/api/auth', authRoutes);
 
 // Global Error Handler
 app.use((err, req, res, next) => {
@@ -53,8 +40,8 @@ app.use((err, req, res, next) => {
 });
 
 app.get("/",(req,res)=>{
-    res.send("Inside the server");
-});
+    res.send("Inside the server")
+})
 
 app.listen(port, () => {
     console.log(`Server running on port ${port}`);
